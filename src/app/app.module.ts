417091b2c784cb02a80090b2a97ba9a59a93be79
Harsh Hundiwala <hhundiwala@gmail.com>
2019-03-28 13:39:32 -0400

    First commit with ng-xs lib installed

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgxsModule } from '@ngxs/store';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+    // NgxsModule.forRoot([])         // use forFeature for lazy loading 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
