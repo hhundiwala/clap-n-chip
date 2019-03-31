35a8af21324fc8f4fb02081ebe75f32e5109801f
Harsh Hundiwala <hhundiwala@gmail.com>
2019-03-30 23:17:00 -0400

    Adding edit portfolio functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ArtistLoginComponent } from './artist-login/artist-login.component';
 import { ArtistPortfolioComponent } from './artist-portfolio/artist-portfolio.component';
+import { EditPortfolioComponent } from './edit-portfolio/edit-portfolio.component';
 
 const routes: Routes = [
   { path: 'artist-signup', component: SignupComponent },
   { path: 'artist-login', component: ArtistLoginComponent },
-  { path: 'artist-portfolio', component: ArtistPortfolioComponent }
+  { path: 'artist-portfolio', component: ArtistPortfolioComponent },
+  { path: 'edit-portfolio', component: EditPortfolioComponent }
 ];
 
 @NgModule({
--- src/app/edit-portfolio/edit-portfolio.component.ts
+++ src/app/edit-portfolio/edit-portfolio.component.ts
@@ -1,4 +1,11 @@
+import { Portfolio } from './../models/portfolio';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Artist } from '../models/artist';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-portfolio',
@@ -7,9 +14,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPortfolioComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private afs: AngularFirestore, private router: Router) { }
+  editPortfolioForm;
+  art: Artist[];
+  
   ngOnInit() {
+    var artist$ = this.afs.collection<Artist>('Artists', ref => ref.where('email', '==', "hhundiwala@gmail.com")).valueChanges();
+     artist$.subscribe((art) => {
+       if(art){
+        this.art = art;
+        if(art[0].portfolio){
+          var port = art[0].portfolio;
+          this.editPortfolioForm =   new FormGroup({
+            story: new FormControl(port.story),
+            values: new FormControl(port.values),
+            note: new FormControl(port.note),
+            youtube_url: new FormControl(port.youtube_url),
+            instagram_url: new FormControl(port.instagram_url),
+            facebook_url: new FormControl(port.facebook_url)
+          });
+        }
+       }
+
+      
+    });
+  }
+
+  updateData(){
+    var id$ = this.afs.collection<Artist>('Artists', ref => ref.where('email', '==', "hhundiwala@gmail.com"))
+    .snapshotChanges()
+    .pipe(map(action => {
+      return action.map(action => {
+        const data = action.payload.doc.data() as Artist;
+        const id = action.payload.doc.id;
+        return { id, data };
+      })
+    }));
+
+    var id;
+    var user;
+    id$.pipe(first()).subscribe((index) => {
+      index.map(data => {
+        id = data.id;
+      })
+      this.afs.collection<Artist>('Artists').doc(id).update({...user, portfolio: this.editPortfolioForm.value});
+      this.router.navigateByUrl('/artist-portfolio');   
+    })
+
+
   }
 
 }
